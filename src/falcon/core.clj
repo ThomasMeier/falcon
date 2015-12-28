b214ccaff20f25519b1eff5cc0094317d93f2baf
Thomas Meier <thomas.meier@citrine.it>
2015-12-28 22:58:48 +0000

    Test html and public function sketch

--- src/falcon/core.clj
+++ src/falcon/core.clj
@@ -1,5 +1,23 @@
 (ns falcon.core
-  (:gen-class))
+  (:import [org.jsoup Jsoup]
+           [org.jsoup.nodes Document]
+           [org.jsoup.select Elements]))
+
+(defn parse-string
+  "Parse HTML from provided string"
+  [html-string]
+  (Jsoup/parse html-string))
+
+(defn parse
+  "Parse HTML from provided URL or File"
+  [url]
+  (parse-string (slurp url)))
+
+(defn sel
+  "Uses JSoup CSS style syntax to select
+  elements from parsed HTML"
+  [parsed selector]
+  (.select parsed selector))
 
 (defn -main
   "I don't do a whole lot ... yet."
