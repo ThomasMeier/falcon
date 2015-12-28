0cb808f4d2518b1a1638d3beb119ac6a6faf2e67
Thomas Meier <thomas.meier@citrine.it>
2015-12-28 20:36:19 +0000

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to falcon
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/falcon/core_test.clj
@@ -0,0 +1,7 @@
+(ns falcon.core-test
+  (:require [clojure.test :refer :all]
+            [falcon.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
