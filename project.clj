8c18b3409d1dad493df8d902bb4b68431ebfb747
Thomas Meier <thomas.meier@citrine.it>
2015-12-29 16:52:28 -0600

    Version number and description update

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject falcon "0.1.0-SNAPSHOT"
-  :description "Fast and simple CSS-based syntax selectors for HTML"
+(defproject falcon "0.1.0"
+  :description "Simple CSS-based selectors for HTML"
   :url "https://github.com/ThomasMeier"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.jsoup/jsoup "1.8.3"]]
